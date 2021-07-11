31b8870f4ee29ab37f4b749f5be5e18a58418fd7
Mariakhanmm92 <mariakhan.mm92@gmail.com>
2021-07-11 15:58:43 +0200

    Added the updated Flow rules for switches

--- Networkslicing_NSH.py
+++ Networkslicing_NSH.py
@@ -47,12 +47,18 @@ sd2 = net.addSwitch('sd2', cls=OVSSwitch, dpid='1000000000000018')	 	#Switch in 
 sd3 = net.addSwitch('sd3', cls=OVSSwitch, dpid='1000000000000019')	 	#Switch in DataCenter3
 
 #Chatroom Service on data centers d1, d2 and d3 is implemented by Sidra Hussain
+#A python script for Chatroom service is deployed on ubuntu:trusty image and configured on the basis of datacenters available in the network emulation
+#In the network topology there are 3 datacenters, so 3 containers for chatroom service are created and deployed on each data center.
+
 info('*** Adding data centers\n')
 d1 = net.addDocker('d1', dimage="mariamajid/chatserver:latest", ip='172.10.0.1/24', mac='00:00:00:00:00:30', dpid='1000000000000017')	#slice1 datacenter
 d2 = net.addDocker('d2', dimage="mariamajid/chatserver2:latest", ip='172.10.0.2/24', mac='00:00:00:00:00:40', dpid='1000000000000018')	#slice2 datacenter
 d3 = net.addDocker('d3', dimage="mariamajid/chatserver3:latest", ip='172.10.0.3/24', mac='00:00:00:00:00:50', dpid='1000000000000019')	#slice3 datacenter
 
 #Possible Extension "DHCP service" on Edge Servers e1, e2 & e3 is implemented by Maria Majid 
+#A package "dnsmasq" is installed on ubuntu:trusty image. The file "dnsmasq.conf" is configured with dhcp listen interface, IP & dhcp range.
+#Similarly, three containers are created for edge server e1, e2, e3, to allocate dynamic IP to their respective slice tenants.
+
 info('*** Adding edge networks\n')
 e1 = net.addDocker('e1', dimage="mariamajid/dhcp1", ip='172.10.0.20/24', mac='00:00:00:00:00:35', dpid='1000000000000005')	#DHCP server for Access nw 1
 e2 = net.addDocker('e2', dimage="mariamajid/dhcp2", ip='172.10.0.30/24', mac='00:00:00:00:00:46', dpid='1000000000000011')	#DHCP server for Access nw 2
@@ -114,7 +120,7 @@ net.addLink(s3, sr3)
 net.addLink(s33, sr3)
 
 #linking transport switches as per the defined path of network slices
-info('*** Creating links with Transport OVSs')
+info('*** Creating links with Transport OVSs\n')
 
 net.addLink(sr1, st1)
 net.addLink(st1, st2)
@@ -142,6 +148,115 @@ net.addLink(st11, sd3)
 
 info('*** Starting network\n')
 net.start()
+
+#Adding Configuration of Flow rules on Open Virtual switches for network slices creation
+#Configurations of slice 1 and 3 are implemented by Maria Majid
+#Configurations of slice 2 are implemented by Sidra Hussain
+
+info('*** Adding Flow rules on Access & Edge Network 1 OVSs\n')
+
+s1.cmd('ovs-ofctl -Oopenflow13 add-flows s1 s1flow.txt')	#configured by maria/sidra
+s11.cmd('ovs-ofctl -Oopenflow13 add-flows s11 s11flow.txt')	#configured by maria/sidra
+sr1.cmd('ovs-ofctl -Oopenflow13 add-flows sr1 sr1flow.txt')	#configured by maria/sidra
+
+info('*** Adding Flow rules on Access & Edge Network 2 OVSs\n')
+
+s2.cmd('ovs-ofctl -Oopenflow13 add-flows s2 s2flow.txt')	#configured by maria
+s22.cmd('ovs-ofctl -Oopenflow13 add-flows s22 s22flow.txt')	#configured by maria
+sr2.cmd('ovs-ofctl -Oopenflow13 add-flows sr2 sr2flow.txt')	#configured by maria
+
+info('*** Adding Flow rules on Access & Edge Network 3 OVSs\n')
+
+s3.cmd('ovs-ofctl -Oopenflow13 add-flows s3 s3flow.txt')	#configured by maria/sidra
+s33.cmd('ovs-ofctl -Oopenflow13 add-flows s33 s33flow.txt')	#configured by maria/sidra
+sr3.cmd('ovs-ofctl -Oopenflow13 add-flows sr3 sr3flow.txt')	#configured by maria/sidra
+
+info('*** Adding Flow rules on Datacenter 1 OVS\n')
+sd1.cmd('ovs-ofctl -Oopenflow13 add-flows sd1 sd1flow.txt')	#configured by maria
+
+info('*** Adding Flow rules on Datacenter 2 OVS\n')
+sd2.cmd('ovs-ofctl -Oopenflow13 add-flows sd2 sd2flow.txt')	#configured by sidra
+
+info('*** Adding Flow rules on Datacenter 3 OVS\n')
+sd3.cmd('ovs-ofctl -Oopenflow13 add-flows sd3 sd3flow.txt')	#configured by maria
+
+info('*** Adding Flow rules on Transport Network OVSs\n')
+
+st1.cmd('ovs-ofctl -Oopenflow13 add-flows st1 st1flow.txt')	#configured by maria/sidra
+st2.cmd('ovs-ofctl -Oopenflow13 add-flows st2 st2flow.txt')	#configured by maria
+st3.cmd('ovs-ofctl -Oopenflow13 add-flows st3 st3flow.txt')	#configured by maria
+st4.cmd('ovs-ofctl -Oopenflow13 add-flows st4 st4flow.txt')	#configured by maria
+st5.cmd('ovs-ofctl -Oopenflow13 add-flows st5 st5flow.txt')	#configured by maria
+st6.cmd('ovs-ofctl -Oopenflow13 add-flows st6 st6flow.txt')	#configured by maria
+st7.cmd('ovs-ofctl -Oopenflow13 add-flows st7 st7flow.txt')	#configured by maria/sidra
+st8.cmd('ovs-ofctl -Oopenflow13 add-flows st8 st8flow.txt')	#configured by maria
+st9.cmd('ovs-ofctl -Oopenflow13 add-flows st9 st9flow.txt')	#configured by sidra
+st10.cmd('ovs-ofctl -Oopenflow13 add-flows st10 st10flow.txt')	#configured by sidra
+st11.cmd('ovs-ofctl -Oopenflow13 add-flows st11 st11flow.txt')	#configured by maria
+st12.cmd('ovs-ofctl -Oopenflow13 add-flows st12 st12flow.txt')	#configured by maria
+
+#Adding Delay
+time.sleep(1)
+
+#Following DHCP configurations are added by  maria
+
+info('***Restarting DHCP service on e1\n')
+#Restarting DHCP service on Edge Server e1
+e1.cmd('service dnsmasq restart')
+
+#Tenants of Access Network 1 getting dynamic IPs from DHCP server e1
+info('*** tenant 1 h11 getting dynamic IP\n')
+h11.cmd('ifconfig h11-eth0 0')
+h11.cmd('dhclient h11-eth0')
+
+info('*** tenant 2 h12 getting dynamic IP\n')
+#In Process
+h12.cmd('ifconfig h12-eth0 0')
+h12.cmd('dhclient h12-eth0')
+
+#Adding Delay
+time.sleep(1)
+
+info('***Restarting DHCP service on e2\n')
+#Restarting DHCP service on Edge Server e2
+e2.cmd('service dnsmasq restart')
+
+#Tenants of Access Network 2 getting dynamic IPs from DHCP server e2
+info('*** tenant 1 h21 getting dynamic IP\n')
+h21.cmd('ifconfig h21-eth0 0')
+h21.cmd('dhclient h21-eth0')
+
+info('*** tenant 2 h22 getting dynamic IP\n')
+#In Process
+h22.cmd('ifconfig h22-eth0 0')
+h22.cmd('dhclient h22-eth0')
+
+#Adding Delay
+time.sleep(1)
+
+info('***Restarting DHCP service on e3\n')
+#Restarting DHCP service on Edge Server e3
+e3.cmd('service dnsmasq restart')
+
+#Tenants of Access Network 3 getting dynamic IPs from DHCP server e3
+info('*** tenant 1 h31 getting dynamic IP\n')
+#In Process
+h31.cmd('ifconfig h31-eth0 0')
+h31.cmd('dhclient h31-eth0')
+
+info('*** tenant 2 h32 getting dynamic IP\n')
+#In Process
+h32.cmd('ifconfig h32-eth0 0')
+h32.cmd('dhclient h32-eth0')
+
+info('*** tenant 3 h33 getting dynamic IP\n')
+#In Process
+h33.cmd('ifconfig h33-eth0 0')
+h33.cmd('dhclient h33-eth0')
+
+time.sleep(1)
+info('*** Welcome to Mobile Computing Project\n')
+
 
 info('*** Running CLI\n')
 CLI(net)
